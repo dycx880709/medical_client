be004a80d5d1bf2dd0d827cbe12df374e418753b
swk@zkm2.com <swk@zkm2.com>
2021-11-14 23:37:24 +0800

    1

---
+++ Mseiot.Medical.Service/Services/BackupProxy.cs
@@ -0,0 +1,27 @@
+﻿using Ms.Libs.Models;
+using Mseiot.Medical.Service.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mseiot.Medical.Service.Services
+{
+    public partial class SocketProxy
+    {
+        public async Task<MsResult<List<SQLInfo>>> GetDBRecords()
+        {
+            return await HttpProxy.GetMessage<List<SQLInfo>>("/api/backup/GetDBRecords");
+        }
+
+
+        public async Task<MsResult<bool>> RemoveDBRecords(List<string> paths)
+        {
+            return await HttpProxy.DeleteMessage<bool>("/api/backup/RemoveDBRecords", new { paths = string.Join(",", paths) });
+        }
+
+
+        
+    }
+}
